2209bd34d04cc74db9628d6b8d33efdf302d3db8
infomanc3r <j.wyatt.ray@gmail.com>
2023-10-03 23:18:34 -0500

    Formatted 'Create gist' page with input boxes for gist description, filename incl extension, and code + create gist button.

--- lib/elixir_gist_clone_web/live/create_gist_live.ex
+++ lib/elixir_gist_clone_web/live/create_gist_live.ex
@@ -1,7 +1,14 @@
 defmodule ElixirGistCloneWeb.CreateGistLive do
   use ElixirGistCloneWeb, :live_view
+  import Phoenix.HTML.Form
+  alias ElixirGistClone.{Gists, Gists.Gist}
 
   def mount(_params, _session, socket) do
+    socket = assign(
+      socket,
+      form: to_form(Gists.change_gist(%Gist{}))
+    )
+
     {:ok, socket}
   end
 
